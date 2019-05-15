2cd763a5bb6e3294c070d2a4a270eaffcbd68dd0
Mahdi Najjarian <mnajarian@gmail.com>
2019-05-15 18:27:58 +0300

    add route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<html><body><h1>Hello World</h1></body></html>')
+})
+
+
 const port = 3001
-
-
 app.listen(port, () => {
-    console.log('server running')
+    console.log('server running on port' + port)
 })
